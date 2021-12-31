a28640fd2372dea7cabcbd7080ba66ef31a5de1a
danielcr0995 <daniel.correa.1995@gmail.com>
2021-12-30 21:39:19 -0500

    truncate post lenght on home page to show only 100 characters

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const date=require(__dirname +'/date.js');
+const _=require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -64,11 +65,18 @@ app.post('/compose', function(req,res){
 })
 
 app.get('/post/:entryTitle',function (req,res){
-  const requestedPost= req.params.entryTitle;
+  const requestedPost= _.replace(_.toLower(req.params.entryTitle)," ","-");
+  // console.log(requestedPost);
   posts.forEach(function(post){
-    savedPost=post.title;
+    const savedPost=_.replace(_.toLower(post.title)," ","-");
     if (savedPost===requestedPost){
-      console.log('Matched');
+      // console.log('Matched');
+      res.render('post',{
+        postTitle:post.title,
+        postDate:post.date,
+        postBody:post.body
+      }
+      )
     };
   })
   
